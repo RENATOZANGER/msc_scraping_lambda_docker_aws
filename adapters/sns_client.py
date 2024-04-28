11dc5a212ddf6c6f13717dab48e6664a1dfa6f71
Renato Zangerolami <renato.zanger@gmail.com>
2024-04-28 19:30:52 -0300

    remove folders

--- adapters/sns_client.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-
-class SNSClientAdapter:
-    def __init__(self, region_name):
-        self.client = boto3.client('sns', region_name=region_name)
-    
-    def publish_message(self, topic_arn, message):
-        self.client.publish(TopicArn=topic_arn, Message=message, MessageStructure='string')
