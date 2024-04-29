683cfdac26249c5ce5111f0c652ced2eca3b42a0
Renato Zangerolami <renato.zanger@gmail.com>
2024-04-29 19:02:00 -0300

    check if lambda exist

--- infra/main.tf
+++ infra/main.tf
@@ -12,10 +12,14 @@ terraform {
 }
 
 resource "aws_lambda_function" "my_lambda" {
-  count         = length(data.aws_lambda_function.existing_lambda.function_name) > 0 ? 0 : 1
+  count         = length(data.aws_lambda_function.this) > 0 ? 0 : 1
   function_name = "Scraping-MSC"
   package_type  = "Image"
   image_uri     = "686148334870.dkr.ecr.us-east-1.amazonaws.com/lambda_scraping:latest"
   role          = aws_iam_role.lambda_role.arn
   timeout       = 90 
+}
+
+data "aws_lambda_function" "this" {
+  function_name = aws_lambda_function.my_lambda.arn
 }
