4f4773290714c807dd4abe740de942186cdaa72d
Renato Zangerolami <renato.zanger@gmail.com>
2024-04-29 18:50:19 -0300

    update name bucket

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "statefile-terraform"
+    bucket         = "statefile-terraform-lambda"
     key            = "path/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = false
