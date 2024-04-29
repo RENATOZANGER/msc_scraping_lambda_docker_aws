5bad910d3b3d8a15fc80a0e7a2bcbc4584f55c58
Renato Zangerolami <renato.zanger@gmail.com>
2024-04-29 17:59:39 -0300

    update image_uri

--- project/infra/main.tf
+++ project/infra/main.tf
@@ -1,12 +1,10 @@
 provider "aws" {
   region = "us-east-1"
-  #access_key = AWS_ACCESS_KEY_ID
-  #secret_key = AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_lambda_function" "my_lambda" {
   function_name = "Scraping-MSC"
-  package_type = "Image"
-  image_uri    = "686148334870.dkr.ecr.us-east-1.amazonaws.com/lambda_scraping:latest"
-  role = aws_iam_role.lambda_execution_role.arn
+  package_type  = "Image"
+  image_uri     = "686148334870.dkr.ecr.us-east-1.amazonaws.com/lambda_scraping:latest"
+  role          = aws_iam_role.lambda_execution_role.arn
 }
