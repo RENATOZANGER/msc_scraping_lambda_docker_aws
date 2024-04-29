e2cd98a1257176a09b8b95efabfa00e6631325e1
Renato Zangerolami <renato.zanger@gmail.com>
2024-04-29 18:46:58 -0300

    update region

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket         = "statefile-terraform"
     key            = "path/terraform.tfstate"
-    region         = var.region
+    region         = "us-east-1"
     encrypt        = false
   }
 }
