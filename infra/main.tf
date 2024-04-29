9784da58413e671cb208e780365c6df8d06576d7
Renato Zangerolami <renato.zanger@gmail.com>
2024-04-29 18:14:00 -0300

    update folders

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_lambda_function" "my_lambda" {
   function_name = "Scraping-MSC"
   package_type  = "Image"
-  image_uri     = "686148334870.dkr.ecr.us-east-1.amazonaws.com/lambda_scraping"
+  image_uri     = "686148334870.dkr.ecr.us-east-1.amazonaws.com/lambda_scraping:latest"
   role          = aws_iam_role.lambda_role.arn
+  timeout       = 90 
 }
