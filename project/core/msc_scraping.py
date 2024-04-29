0c343d84303b4bb2c8d17664d4f9e8a6aa4ac95a
Renato Zangerolami <renato.zanger@gmail.com>
2024-04-29 16:16:50 -0300

    refactoring

--- project/core/msc_scraping.py
+++ project/core/msc_scraping.py
@@ -4,6 +4,7 @@ from core.scraping_service import ScrapingService
 from adapters.chrome_driver import ChromeDriverAdapter, get_chrome_options
 from adapters.sns_client import SNSClientAdapter
 
+
 class MscScraping:
     def __init__(self):
         self.chrome_driver = ChromeDriverAdapter("/opt/chromedriver")
@@ -17,9 +18,10 @@ class MscScraping:
             driver = self.chrome_driver.get_driver(options)
             scraping_service = ScrapingService(driver)
             results = scraping_service.scrape_website()
-            target_value = scraping_service.process_results(results)
+            target_value = [results[result] for result in results if (float(result) < 5.800)]
+            self.logging.info(target_value)
             if target_value:
                 message = json.dumps(target_value, indent=4, ensure_ascii=False)
-                self.sns_client.publish_message('SNS_ARN', message)
-        finally:
-            driver.quit()
+                self.sns_client.publish_message('ARN_SNS', message)
+        except Exception as e:
+            self.logging.error(f"Error: {str(e)}")
--- project/core/scraping_service.py
+++ project/core/scraping_service.py
@@ -1,5 +1,8 @@
 import re
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import logging
 
 area = "SOA"  # Region: South America
@@ -19,35 +22,33 @@ class ScrapingService:
         self.driver.get(
             "https://www.msccruzeiros.com.br/Search%20Result?area=SOA&embkPort=SSZ&departureDateFrom=01%2F01%2F2025"
             "&departureDateTo=31%2F01%2F2025&passengers=2%7C0%7C0%7C0&page=1&nights=6%2C7")
-        self.driver.implicitly_wait(5)
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "itinerary-card-detail__destination"))
+            )
+            destinations = self.driver.find_elements(By.CLASS_NAME, "itinerary-card-detail__destination")
+            durations = self.driver.find_elements(By.CLASS_NAME, "itinerary-card-detail__duration")
+            ship_names = self.driver.find_elements(By.CLASS_NAME, "itinerary-card-detail__ship-name-and-itinerary")
+            prices = (self.driver.find_elements(By.CLASS_NAME, "itinerary-card-price__price"))
+            ports = self.driver.find_elements(By.CLASS_NAME, "itinerary-card-detail__port-name")
         
-        destinations = self.driver.find_elements(By.CLASS_NAME, "itinerary-card-detail__destination")
-        durations = self.driver.find_elements(By.CLASS_NAME, "itinerary-card-detail__duration")
-        ship_names = self.driver.find_elements(By.CLASS_NAME, "itinerary-card-detail__ship-name-and-itinerary")
-        prices = (self.driver.find_elements(By.CLASS_NAME, "itinerary-card-price__price"))
-        ports = self.driver.find_elements(By.CLASS_NAME, "itinerary-card-detail__port-name")
+            results = {}
         
-        results = {}
-        
-        for destination, duration, ship_name, price, port in zip(destinations, durations, ship_names, prices, ports):
-            value = re.findall(r'\d|\.', price.text)
-            formatted_value = ''.join(value)
-            result = {
-                "Destination": destination.text,
-                "Duration": duration.text,
-                "Ship_Name": ship_name.text,
-                "Boarding": port.text,
-                "Price": price.text
-            }
-            results[formatted_value] = result
-        return results
-    
-    def process_results(self, results):
-        target_value = []
-        for result in results:
-            if float(result) < 5.800:
-                target_value.append(results[result])
-                self.logging.info(results[result])
-            else:
-                self.logging.info(results[result])
-        return target_value
+            for destination, duration, ship_name, price, port in zip(destinations, durations, ship_names, prices, ports):
+                value = re.findall(r'\d|\.', price.text)
+                formatted_value = ''.join(value)
+                result = {
+                    "Destination": destination.text,
+                    "Duration": duration.text,
+                    "Ship_Name": ship_name.text,
+                    "Boarding": port.text,
+                    "Price": price.text
+                }
+                results[formatted_value] = result
+            return results
+        except TimeoutException:
+            self.logging.warning("Element not found")
+        except Exception as e:
+            self.logging.error(f"Error: {str(e)}")
+        finally:
+            self.driver.quit()
