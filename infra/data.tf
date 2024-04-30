ac16ffd925063d5e5946b6b1948414ddfb158db0
Renato Zangerolami <renato.zanger@gmail.com>
2024-04-30 12:40:02 -0300

    update variables

--- infra/data.tf
+++ infra/data.tf
@@ -1,5 +1,5 @@
 data "aws_caller_identity" "current" {}
 
 data "external" "github_actions_variables" {
-  program = ["bash", "-c", "echo \"name_repo=${NAME_REPO}\""]
+  program = ["bash", "-c", "echo \"{\\\"name_repo\\\": \\\"${var.Name_Repo}\\\"}\""]
 }
