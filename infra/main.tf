18751c49412a52cfd7762e862d4d9a9375d31ff0
Renato Zangerolami <renato.zanger@gmail.com>
2024-04-30 12:53:28 -0300

    include depends on

--- infra/main.tf
+++ infra/main.tf
@@ -11,6 +11,7 @@ terraform {
 }
 
 resource "aws_lambda_function" "my_lambda" {
+  depends_on = [ aws_sns_topic_subscription.email_subscription ]
   function_name = var.lambda_name
   package_type  = "Image"
   image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/lambda_scraping:latest"
