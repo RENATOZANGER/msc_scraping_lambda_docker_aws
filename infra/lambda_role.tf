40e066b9a72dadacff078f2983bf436d0a941abe
Renato Zangerolami <renato.zanger@gmail.com>
2024-05-01 22:36:41 -0300

    include terraform format

--- infra/lambda_role.tf
+++ infra/lambda_role.tf
@@ -2,10 +2,10 @@ resource "aws_iam_role" "lambda_role" {
   name = var.role_name
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
+        Action    = "sts:AssumeRole"
         Principal = {
           Service = ["lambda.amazonaws.com","scheduler.amazonaws.com"]
         }
--- infra/scheduler.tf
+++ infra/scheduler.tf
@@ -1,16 +1,16 @@
 resource "aws_scheduler_schedule" "scheduler_lambda" {
-    name = "scheduler-Lambda-MSC"
-    group_name = "default"
+    name                         = "scheduler-Lambda-MSC"
+    group_name                   = "default"
     schedule_expression_timezone = "America/Sao_Paulo"
-    state = "ENABLED"
+    state                        = "ENABLED"
     flexible_time_window {
       mode = "OFF"
     }
 
-    schedule_expression = "rate(30 minutes)"
+    schedule_expression          = "rate(30 minutes)"
 
     target {
-      arn = aws_lambda_function.my_lambda.arn
+      arn      = aws_lambda_function.my_lambda.arn
       role_arn = aws_iam_role.lambda_role.arn
     }
 }
