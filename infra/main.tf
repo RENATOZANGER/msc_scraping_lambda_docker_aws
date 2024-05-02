950c9beef1e6db4af6ffbe09f873b2f942fb62e5
Renato Zangerolami <renato.zanger@gmail.com>
2024-05-01 22:06:03 -0300

    test variable

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket = "lambda-scraping-remote-state"
     key = "lambda-scraping/terraform.tfstate"
-    region = var.test_region
+    region = "us-east-1"
   }
 }
 
@@ -14,7 +14,7 @@ resource "aws_lambda_function" "my_lambda" {
   depends_on = [ aws_sns_topic_subscription.email_subscription ]
   function_name = var.lambda_name
   package_type  = "Image"
-  image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/${var.repo_name}:latest"
+  image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-${var.test_region}.amazonaws.com/${var.repo_name}:latest"
   role          = aws_iam_role.lambda_role.arn
   timeout       = 90
   memory_size = 1024
